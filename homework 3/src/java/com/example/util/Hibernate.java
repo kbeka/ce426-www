ce06daa01bf4532197a325341a3611f3961e8988
Cleo Beka <57142768+kbeka@users.noreply.github.com>
2020-11-12 23:05:21 +0200

    Add files via upload

---
+++ homework 3/src/java/com/example/util/Hibernate.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.util;
+
+/**
+ *
+ * @author cleob
+ */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author cleob
+ */
+
+import java.util.Properties;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+import com.example.model.*;
+import org.hibernate.HibernateException;
+
+/**
+ * Java based configuration
+ * @author ramesh Fadatare
+ *
+ */
+public class Hibernate {
+	private static SessionFactory sessionFactory;
+
+	public static SessionFactory getSessionFactory() {
+		if (sessionFactory == null) {
+			try {
+				Configuration configuration = new Configuration();
+
+				// Hibernate settings equivalent to hibernate.cfg.xml's properties
+				Properties settings = new Properties();
+				settings.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
+				settings.put(Environment.URL, "jdbc:mysql://localhost:3306/mydb");
+				settings.put(Environment.USER, "root");
+				settings.put(Environment.PASS, "BubbleGum");
+				settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+
+				settings.put(Environment.SHOW_SQL, "true");
+
+				settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+
+				settings.put(Environment.HBM2DDL_AUTO, "update");
+
+				configuration.setProperties(settings);
+				configuration.addAnnotatedClass(Products.class);
+
+				ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+						.applySettings(configuration.getProperties()).build();
+				System.out.println("Hibernate Java Config serviceRegistry created");
+				sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+				return sessionFactory;
+
+			} catch (HibernateException e) {
+			}
+		}
+		return sessionFactory;
+	}
+}
